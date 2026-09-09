47d23e31f0a237d5692a2bc29f9d58a7a19976a1
Rahul Rajput <rahulrajput.codes@gmail.com>
2026-09-09 18:43:28 +0530

    exception and files handling in py

---
+++ Python/exceptionHandling.py
@@ -0,0 +1,45 @@
+# a = 12 
+
+# if a == 12:
+#     print("hello")
+
+
+# a = 10 
+# b = int(input("please tell a number :- "))
+
+# print(a/b)
+
+# a = "10"
+# b = 5 
+# print(a + b)
+
+# num = int("hello")
+
+
+# a = int(input("please tell your 1st number:-  "))
+# b = int(input("please tell your 2nd number:-  "))
+
+# try:
+#     print(a/b)
+# except Exception as err:
+#     print(f"Sorry an error occured as {err}")
+
+# finally:
+#     print("if there are errors or there are no errors I will run no matter what ")
+
+
+
+# name = input("tell your name :- ")
+
+# print(f"Hello your name is {name}")
+
+
+# age = int(input("tell your age :- "))
+
+# if age < 18:
+#     raise TypeError("you are not eligible")
+
+# print("you are eligible")
+
+
+# else block is also there , it runs only if no exception occurs 
---
+++ Python/fileHandling.py
@@ -0,0 +1,15 @@
+# open("hello.txt","x") #this will create
+
+# file = open("superman.txt","w")
+
+# data = input("what you want to write in your file :- ")
+
+# file.write(data)
+
+# file = open("numbergame.py","r")
+
+# print(file.read())
+
+
+# with open("superman.txt","a") as f:
+#     f.write(" " + "i want to see if it is working or not")
